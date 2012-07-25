23b51e7f6c2667c857551a7128dbceeedfc41a22
Lars Brinkhoff <lars@nocrew.org>
2012-07-25 12:29:53 +0200

    stuff

---
+++ utils.lisp
@@ -0,0 +1,14 @@
+(defpackage #:se.brinkhoff.colorimetric-utils
+  (:use #:cl))
+
+(in-package #:se.brinkhoff.colorimetric-utils)
+
+(defmacro define-simple-array-constant (name type &body value)
+  `(progn
+     (declaim (type (simple-array ,type (,(length value))) ,name))
+     (defconstant ,name
+       ,(make-array
+	 (length value)
+	 :element-type type
+	 :initial-contents (loop for x in value collect (coerce x type))))))
+
